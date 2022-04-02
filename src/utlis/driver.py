32cf492f5c43cf3d80029788f23f60ed5ebe5939
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2022-04-02 11:13:53 +0800

    Add cache architecture to datasets

--- src/utlis/driver.py
+++ src/utlis/driver.py
@@ -1,11 +1,12 @@
 import pickle
 from abc import ABC
-from typing import Dict
+from typing import Dict, Union
 
 import networkx as nx
 import numpy as np
 import torch
 from torch.utils.data import Dataset
+import tqdm
 
 
 class NetworkDataset(Dataset, ABC):
@@ -21,30 +22,40 @@ class NetworkDataset(Dataset, ABC):
     u_tc: torch.Tensor
     v_tc: torch.Tensor
     y_tc: torch.Tensor
+    cache: Union[None, np.ndarray]
 
-    def __init__(self, dataset_path: str):
+    def __init__(self, dataset_path: str, build_cache: bool = True):
         super().__init__()
         self.dataset_path: str = dataset_path
         self.data = self.unpickle(self.dataset_path)
         self.unpack()
         assert len(self.authors) == len(self.abstracts) == len(self.graph.nodes()), ValueError(f"Data not aligned")
-
-        pass
+        self.cache = self.data['cache'] if 'cache' in self.data.keys() else None
+        if build_cache and self.cache is None:
+            self.build_cache()
 
     @classmethod
     def unpickle(cls, dataset_path: str) -> Dict[str, np.ndarray]:
         with open(dataset_path, 'rb') as f:
             return pickle.load(f)
 
+    @property
+    def u(self):
+        return self.data['u']
+
+    @property
+    def v(self):
+        return self.data['v']
+
+    @property
+    def y(self):
+        return self.data['y']
+
     def unpack(self):
         """
         This function prepare the train/test/eval dataset
         :return:
         """
-        self.u = np.concatenate([self.data['pos_u'], self.data['neg_u']])
-        self.v = np.concatenate([self.data['pos_v'], self.data['neg_v']])
-        self.y = np.concatenate([np.ones(shape=len(self.data['pos_u']), dtype=int), np.zeros(shape=len(self.data['pos_u']), dtype=int)])
-
         self.u_tc = torch.tensor(self.u, dtype=torch.float32)
         self.v_tc = torch.tensor(self.v, dtype=torch.float32)
         self.y_tc = torch.tensor(self.y, dtype=torch.float32)
@@ -54,10 +65,23 @@ class NetworkDataset(Dataset, ABC):
         self.abstracts = self.data['abstracts']
         self.length = self.u.shape[0]
 
+    def build_cache(self):
+        cache = np.zeros(shape=(self.length, self.feature_dim + 1), dtype=np.float32)
+        with tqdm.tqdm(range(self.length)) as pbar:
+            for idx in range(self.length):
+                cache[idx] = self[idx]
+                pbar.update()
+        self.cache = cache
+        self.data['cache'] = cache
+        with open(self.dataset_path, 'wb') as f:
+            pickle.dump(self.data, f)
+
+    feature_dim: int = 5  # __getitem__ output dim = feature_dim + 1
+
     def get_item_baseline(self, item):
         u, v, y = self.u[item], self.v[item], self.y[item]
-        u_degree = self.graph.degree(u)
-        v_degree = self.graph.degree(v)
+        u_degree = int(self.graph.degree(u))
+        v_degree = int(self.graph.degree(v))
         u_abstract = self.abstracts[u]
         v_abstract = self.abstracts[v]
 
@@ -74,14 +98,21 @@ class NetworkDataset(Dataset, ABC):
         :param item:
         :return:
         """
-        return self.get_item_baseline(item)
+        if self.cache is not None:
+            return self.cache[item]
+        else:
+            return self.get_item_baseline(item)
 
     def __len__(self):
         return self.length
 
 
 if __name__ == '__main__':
-    driver = NetworkDataset('../../data/converted/nullptr_train.pkl')
-    print(driver[1000])
-    print(len(driver))
+    train_driver = NetworkDataset('../../data/converted/nullptr_train.pkl')
+    dev_driver = NetworkDataset('../../data/converted/nullptr_dev.pkl')
+    test_driver = NetworkDataset('../../data/converted/nullptr_test.pkl')
+    whole_driver = NetworkDataset('../../data/converted/nullptr_whole.pkl')
+
+    print(train_driver[1000:1100])
+    print(len(train_driver))
     print('finish')
--- tasks/generate_dataset/generate_dataset.py
+++ tasks/generate_dataset/generate_dataset.py
@@ -7,6 +7,8 @@ import networkx as nx
 import numpy as np
 import tqdm
 
+from src.utlis import NetworkDataset
+
 
 def read_abstracts(abstracts_path: str) -> np.ndarray:
     def split_fn(line: str) -> List[str]:
@@ -19,7 +21,7 @@ def read_abstracts(abstracts_path: str) -> np.ndarray:
     return np.array(list(result), dtype=object)
 
 
-def read_author(authors_path: str) -> np.ndarray:
+def read_authors(authors_path: str) -> np.ndarray:
     def split_fn(line: str) -> List[str]:
         _, authors = line.split('|--|')
         return authors.split(',')
@@ -32,6 +34,16 @@ def read_author(authors_path: str) -> np.ndarray:
 
 def read_graph(edges_path: str, nodetype=int) -> nx.Graph:
     return nx.read_edgelist(edges_path, delimiter=',', create_using=nx.Graph(), nodetype=nodetype)
+
+
+def read_evaluate_cases(test_path: str) -> np.ndarray:
+    node_pairs = list()
+    with open(test_path, 'r') as f:
+        for line in f:
+            t = line.split(',')
+            node_pairs.append((int(t[0]), int(t[1])))
+
+    return np.array(node_pairs)
 
 
 # @numba.jit(nopython=True)
@@ -51,6 +63,12 @@ def select_neg_edges(G: nx.Graph,
     return neg_u, neg_v
 
 
+def create_features(G: nx.Graph,
+                    num_edges: int):
+    with tqdm.tqdm(range(num_edges)):
+        pass
+
+
 if __name__ == '__main__':
     # >>>>>> Beg modify parameters >>>>>>
     RECORD_NAMES: Dict[str, str] = {
@@ -63,23 +81,27 @@ if __name__ == '__main__':
     DATASET_PATH = input("Input the path of dataset") if len(sys.argv) < 2 else sys.argv[1]
     OUTPUT_PATH = input("Input the path of output") if len(sys.argv) < 3 else sys.argv[2]
     SEED = 0
+    PREFIX: str = 'nullptr'
     # <<<<<< End modify parameters <<<<<<
     np.random.seed(SEED)
     if not os.path.exists(OUTPUT_PATH):
         os.makedirs(OUTPUT_PATH)
 
+    whole_set: Dict[str, np.ndarray] = {}
     train_set: Dict[str, np.ndarray] = {}
     dev_set: Dict[str, np.ndarray] = {}
+    test_set: Dict[str, np.ndarray] = {}
 
     # Assign common information
     dataset_path: str = DATASET_PATH
     graph = nx.Graph = read_graph(os.path.join(DATASET_PATH, RECORD_NAMES['edges']))
     abstracts: np.ndarray = read_abstracts(os.path.join(DATASET_PATH, RECORD_NAMES['abstracts']))
-    authors: np.ndarray = read_author(os.path.join(DATASET_PATH, RECORD_NAMES['authors']))
+    authors: np.ndarray = read_authors(os.path.join(DATASET_PATH, RECORD_NAMES['authors']))
+    evaluate_cases: np.ndarray = read_evaluate_cases(os.path.join(DATASET_PATH, RECORD_NAMES['evaluate_cases']))
     edges: np.ndarray = np.array(graph.edges())
-    train_set['abstracts'], dev_set['abstracts'] = abstracts, abstracts
-    train_set['authors'], dev_set['authors'] = authors, authors
-    train_set['origin_edges'], dev_set['origin_edges'] = edges, edges
+    train_set['abstracts'], dev_set['abstracts'], test_set['abstracts'], whole_set['abstracts'] = abstracts, abstracts, abstracts, abstracts
+    train_set['authors'], dev_set['authors'], test_set['authors'], whole_set['authors'] = authors, authors, authors, authors
+    train_set['origin_edges'], dev_set['origin_edges'], test_set['origin_edges'], whole_set['origin_edges'] = edges, edges, edges, edges
 
     assert len(authors) == len(abstracts) == len(graph), ValueError(f"Data not aligned")
 
@@ -98,6 +120,7 @@ if __name__ == '__main__':
     pos_u, pos_v = edges[:, 0], edges[:, 1]
     train_set['pos_u'], train_set['pos_v'] = pos_u[pos_edge_ids[:train_size]], pos_v[pos_edge_ids[:train_size]]
     dev_set['pos_u'], dev_set['pos_v'] = pos_u[pos_edge_ids[train_size:]], pos_v[pos_edge_ids[train_size:]]
+    whole_set['pos_u'], whole_set['pos_v'] = pos_u[pos_edge_ids], pos_v[pos_edge_ids]
 
     # Sample negative cases
     # adj = sp.coo_matrix((np.ones(len(pos_u)), (pos_u, pos_v)))
@@ -107,15 +130,40 @@ if __name__ == '__main__':
     neg_u, neg_v = select_neg_edges(graph, graph.number_of_edges())
     train_set['neg_u'], train_set['neg_v'] = neg_u[neg_edge_ids[:train_size]], neg_v[neg_edge_ids[:train_size]]
     dev_set['neg_u'], dev_set['neg_v'] = neg_u[neg_edge_ids[train_size:]], neg_v[neg_edge_ids[train_size:]]
+    whole_set['neg_u'], whole_set['neg_v'] = neg_u[neg_edge_ids], neg_v[neg_edge_ids]
+
+    train_set['u'] = np.concatenate([train_set['pos_u'], train_set['neg_u']])
+    train_set['v'] = np.concatenate([train_set['pos_v'], train_set['neg_v']])
+    train_set['y'] = np.concatenate([np.ones(shape=len(train_set['pos_u']), dtype=int), np.zeros(shape=len(train_set['pos_u']), dtype=int)])
+
+    dev_set['u'] = np.concatenate([dev_set['pos_u'], dev_set['neg_u']])
+    dev_set['v'] = np.concatenate([dev_set['pos_v'], dev_set['neg_v']])
+    dev_set['y'] = np.concatenate([np.ones(shape=len(dev_set['pos_u']), dtype=int), np.zeros(shape=len(dev_set['pos_u']), dtype=int)])
+
+    test_set['u'] = evaluate_cases[:, 0]
+    test_set['v'] = evaluate_cases[:, 1]
+    test_set['y'] = - np.ones_like(test_set['u'])  # The label of test_set is unknown
+
+    whole_set['u'] = np.concatenate([whole_set['pos_u'], whole_set['neg_u']])
+    whole_set['v'] = np.concatenate([whole_set['pos_v'], whole_set['neg_v']])
+    whole_set['y'] = np.concatenate([np.ones(shape=len(whole_set['pos_u']), dtype=int), np.zeros(shape=len(whole_set['pos_u']), dtype=int)])
 
     # Saving
     print(f"Saving to {OUTPUT_PATH}")
-    np.savez(os.path.join(OUTPUT_PATH, f'nullptr_train.npz'), **train_set)
-    np.savez(os.path.join(OUTPUT_PATH, f'nullptr_dev.npz'), **dev_set)
+    np.savez(os.path.join(OUTPUT_PATH, f'{PREFIX}_train.npz'), **train_set)
+    np.savez(os.path.join(OUTPUT_PATH, f'{PREFIX}_dev.npz'), **dev_set)
+    np.savez(os.path.join(OUTPUT_PATH, f'{PREFIX}_test.npz'), **test_set)
+    np.savez(os.path.join(OUTPUT_PATH, f'{PREFIX}_whole.npz'), **whole_set)
 
-    with open(os.path.join(OUTPUT_PATH, f'nullptr_train.pkl'), 'wb') as f:
+    with open(os.path.join(OUTPUT_PATH, f'{PREFIX}_train.pkl'), 'wb') as f:
         pickle.dump(train_set, f)
-    with open(os.path.join(OUTPUT_PATH, f'nullptr_dev.pkl'), 'wb') as f:
+    with open(os.path.join(OUTPUT_PATH, f'{PREFIX}_dev.pkl'), 'wb') as f:
         pickle.dump(dev_set, f)
+    with open(os.path.join(OUTPUT_PATH, f'{PREFIX}_test.pkl'), 'wb') as f:
+        pickle.dump(test_set, f)
+    with open(os.path.join(OUTPUT_PATH, f'{PREFIX}_whole.pkl'), 'wb') as f:
+        pickle.dump(test_set, f)
 
+    print(f"Generating Cache")
+    [NetworkDataset(os.path.join(OUTPUT_PATH, f"{PREFIX}_{name}.pkl")) for name in ['train', 'dev', 'test', 'whole']]
     pass
