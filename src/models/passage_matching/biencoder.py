af2a9b183d88de944d984e354638fbf971fa8b96
WANG Jinyuan <WangSteve@icloud.com>
2022-04-09 16:59:58 +0800

    Commit info: - biencoder.py: Change BiEncoder to AutoBiEncoder with multiple transformer model support; Adopt 3 types of AutoBiEncoder [cat, sum, product] to find a better matching solution - trainer.py: Now initialize biencoder with machine-specified config files - config/machine: Machine-specified config files

--- src/models/passage_matching/biencoder.py
+++ src/models/passage_matching/biencoder.py
@@ -11,7 +11,7 @@ from .data_utils import Tensorizer
 
 
 # BiEncoderPassage = collections.namedtuple("BiEncoderPassage", ["authors", "abstract"])
-
+from transformers import AutoModel, AutoConfig, AutoTokenizer
 
 BiEncoderBatch = collections.namedtuple(
     "BiEncoderInput",
@@ -35,6 +35,252 @@ def dot_product_scores(q_vectors: T, p_vectors: T):
     """
     result = torch.matmul(q_vectors, torch.transpose(p_vectors, 0, 1))
     return result
+
+
+class AutoBiEncoderCat(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.config = AutoConfig.from_pretrained(cfg.model_name_or_path)
+        self.query_model: nn.Module = AutoModel.from_pretrained(cfg.model_name_or_path, config = self.config)
+        self.context_model: nn.Module = AutoModel.from_pretrained(cfg.model_name_or_path, config = self.config)
+        self.classifier: nn.Linear = nn.Linear(self.config.hidden_size*2, 2)
+
+    def forward(
+            self,
+            query_ids: T,
+            query_segments: T,
+            query_attention_mask: T,
+            context_ids: T,
+            context_segments: T,
+            context_attention_mask: T,
+            encoder_type: str = None,
+            representation_token_pos: int = 0,
+    ) -> Tuple[T, T]:
+        query_output = self.query_model(
+            input_ids=query_ids,
+            token_type_ids=query_segments,
+            attention_mask=query_attention_mask,
+        )
+        context_output = self.context_model(
+            input_ids=context_ids,
+            token_type_ids=context_segments,
+            attention_mask=context_attention_mask,
+        )
+        query_pooled = query_output.last_hidden_state[:, 0, :]
+        context_pooled = context_output.last_hidden_state[:, 0, :]
+        output = self.classifier(torch.cat([query_pooled, context_pooled], dim=1))
+
+        return output
+
+    def create_biencoder_input(
+            self,
+            batch: Dict,
+            tensorizer: Tensorizer,
+    ) -> BiEncoderBatch:
+
+        query_passages: Dict = batch["query"]
+        context_passages: Dict = batch["context"]
+        labels: T = batch["label"]
+
+        query_tensors = []
+        context_tensors = []
+
+        for i in range(len(labels)):
+            query_tensors.append(tensorizer.text_to_tensor(query_passages["abstract"][i]))
+            context_tensors.append(tensorizer.text_to_tensor(context_passages["abstract"][i]))
+
+        # passage_tensors.size() = [batch_size, max_seq_len]
+        context_tensors = torch.cat([ctx.view(1, -1) for ctx in context_tensors], dim=0)
+        # query_tensors.size() = [batch_size, max_seq_len]
+        query_tensors = torch.cat([q.view(1, -1) for q in query_tensors], dim=0)
+
+        context_segments = torch.zeros_like(context_tensors)
+        query_segments = torch.zeros_like(query_tensors)
+
+        return BiEncoderBatch(
+            query_ids=query_tensors,
+            query_segments=query_segments,
+            context_ids=context_tensors,
+            context_segments=context_segments,
+            labels=labels,
+            encoder_type="query"
+        )
+
+    def load_state(self, saved_state: CheckpointState, strict: bool = True):
+        self.load_state_dict(saved_state.model_dict, strict=strict)
+
+    def get_state_dict(self):
+        return self.state_dict()
+
+
+
+class AutoBiEncoderSum(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.config = AutoConfig.from_pretrained(cfg.model_name_or_path)
+        self.query_model: nn.Module = AutoModel.from_pretrained(cfg.model_name_or_path, config = self.config)
+        self.context_model: nn.Module = AutoModel.from_pretrained(cfg.model_name_or_path, config = self.config)
+        self.classifier: nn.Linear = nn.Linear(self.config.hidden_size, 2)
+
+    def forward(
+            self,
+            query_ids: T,
+            query_segments: T,
+            query_attention_mask: T,
+            context_ids: T,
+            context_segments: T,
+            context_attention_mask: T,
+            encoder_type: str = None,
+            representation_token_pos: int = 0,
+    ) -> Tuple[T, T]:
+        query_output = self.query_model(
+            input_ids=query_ids,
+            token_type_ids=query_segments,
+            attention_mask=query_attention_mask,
+        )
+        context_output = self.context_model(
+            input_ids=context_ids,
+            token_type_ids=context_segments,
+            attention_mask=context_attention_mask,
+        )
+        query_pooled = query_output.last_hidden_state[:, 0, :]
+        context_pooled = context_output.last_hidden_state[:, 0, :]
+        output = self.classifier(query_pooled + context_pooled)
+
+        return output
+
+    def create_biencoder_input(
+            self,
+            batch: Dict,
+            tensorizer: Tensorizer,
+    ) -> BiEncoderBatch:
+
+        query_passages: Dict = batch["query"]
+        context_passages: Dict = batch["context"]
+        labels: T = batch["label"]
+
+        query_tensors = []
+        context_tensors = []
+
+        for i in range(len(labels)):
+            query_tensors.append(tensorizer.text_to_tensor(query_passages["abstract"][i]))
+            context_tensors.append(tensorizer.text_to_tensor(context_passages["abstract"][i]))
+
+        # passage_tensors.size() = [batch_size, max_seq_len]
+        context_tensors = torch.cat([ctx.view(1, -1) for ctx in context_tensors], dim=0)
+        # query_tensors.size() = [batch_size, max_seq_len]
+        query_tensors = torch.cat([q.view(1, -1) for q in query_tensors], dim=0)
+
+        context_segments = torch.zeros_like(context_tensors)
+        query_segments = torch.zeros_like(query_tensors)
+
+        return BiEncoderBatch(
+            query_ids=query_tensors,
+            query_segments=query_segments,
+            context_ids=context_tensors,
+            context_segments=context_segments,
+            labels=labels,
+            encoder_type="query"
+        )
+
+    def load_state(self, saved_state: CheckpointState, strict: bool = True):
+        self.load_state_dict(saved_state.model_dict, strict=strict)
+
+    def get_state_dict(self):
+        return self.state_dict()
+
+
+class AutoBiEncoderProduct(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.config = AutoConfig.from_pretrained(cfg.model_name_or_path)
+        self.query_model: nn.Module = AutoModel.from_pretrained(cfg.model_name_or_path, config = self.config)
+        self.context_model: nn.Module = AutoModel.from_pretrained(cfg.model_name_or_path, config = self.config)
+        self.classifier: nn.Linear = nn.Linear(self.config.hidden_size, 2)
+
+    def forward(
+            self,
+            query_ids: T,
+            query_segments: T,
+            query_attention_mask: T,
+            context_ids: T,
+            context_segments: T,
+            context_attention_mask: T,
+            encoder_type: str = None,
+            representation_token_pos: int = 0,
+    ) -> Tuple[T, T]:
+        query_output = self.query_model(
+            input_ids=query_ids,
+            token_type_ids=query_segments,
+            attention_mask=query_attention_mask,
+        )
+        context_output = self.context_model(
+            input_ids=context_ids,
+            token_type_ids=context_segments,
+            attention_mask=context_attention_mask,
+        )
+        query_pooled = query_output.last_hidden_state[:, 0, :]
+        context_pooled = context_output.last_hidden_state[:, 0, :]
+        output = self.classifier(query_pooled * context_pooled)
+
+        return output
+
+    def create_biencoder_input(
+            self,
+            batch: Dict,
+            tensorizer: Tensorizer,
+    ) -> BiEncoderBatch:
+
+        query_passages: Dict = batch["query"]
+        context_passages: Dict = batch["context"]
+        labels: T = batch["label"]
+
+        query_tensors = []
+        context_tensors = []
+
+        for i in range(len(labels)):
+            query_tensors.append(tensorizer.text_to_tensor(query_passages["abstract"][i]))
+            context_tensors.append(tensorizer.text_to_tensor(context_passages["abstract"][i]))
+
+        # passage_tensors.size() = [batch_size, max_seq_len]
+        context_tensors = torch.cat([ctx.view(1, -1) for ctx in context_tensors], dim=0)
+        # query_tensors.size() = [batch_size, max_seq_len]
+        query_tensors = torch.cat([q.view(1, -1) for q in query_tensors], dim=0)
+
+        context_segments = torch.zeros_like(context_tensors)
+        query_segments = torch.zeros_like(query_tensors)
+
+        return BiEncoderBatch(
+            query_ids=query_tensors,
+            query_segments=query_segments,
+            context_ids=context_tensors,
+            context_segments=context_segments,
+            labels=labels,
+            encoder_type="query"
+        )
+
+    def load_state(self, saved_state: CheckpointState, strict: bool = True):
+        self.load_state_dict(saved_state.model_dict, strict=strict)
+
+    def get_state_dict(self):
+        return self.state_dict()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class BiEncoder(nn.Module):
