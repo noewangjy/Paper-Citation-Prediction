bd27d8744cf800bf33f30b6b841d1c75eb29250b
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2022-04-13 13:43:34 +0800

    GraphVAE cannot be efficiently trained due to memory limit TODO: Find a way to apply cross entropy on sparse tensors

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from .PseudoModel import PseudoModel
 from .GraphSAGE import MLPPredictor, DotPredictor, GraphSAGEBert, GraphSAGEBundled
 from .MLPBert import MLPBert
+from .GraphVAE import GATModelVAE
